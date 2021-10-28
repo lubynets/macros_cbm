9d5ca3b578fd8c4179e001624f879f4cf105354a
Lubynets <lubinets95@gmail.com>
2021-10-28 15:46:47 +0200

    add counter script

---
+++ counter_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd /lustre/cbm/users/lubynets/atfiller
+pwd ; find . -type f | wc -l
+cd /lustre/cbm/users/lubynets/batch
+pwd ; find . -type f | wc -l
+cd /lustre/cbm/users/lubynets/cbm2atree
+pwd ; find . -type f | wc -l
+cd /lustre/cbm/users/lubynets/CbmKF
+pwd ; find . -type f | wc -l
+cd /lustre/cbm/users/lubynets/KFUnitTest
+pwd ; find . -type f | wc -l
+cd /lustre/cbm/users/lubynets/mydir
+pwd ; find . -type f | wc -l
+cd /lustre/cbm/users/lubynets/pfsimple
+pwd ; find . -type f | wc -l
+cd /lustre/cbm/users/lubynets/QA
+pwd ; find . -type f | wc -l
+cd /lustre/cbm/users/lubynets/qna
+pwd ; find . -type f | wc -l
+cd /lustre/cbm/users/lubynets/soft
+pwd ; find . -type f | wc -l
+cd /lustre/cbm/users/lubynets/WORKDIR
+pwd ; find . -type f | wc -l
+
