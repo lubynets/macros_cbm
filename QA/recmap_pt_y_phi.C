832ffce61493e5b47d41e30e582ea89237fa4adb
Oleksii Lubynets <lubynets@lxbuild07.gsi.de>
2021-11-02 12:08:27 +0100

    improve mapper

--- QA/recmap_pt_y_phi.C
+++ QA/recmap_pt_y_phi.C
@@ -1,64 +1,95 @@
-void recmap_pt_y_phi(const TString recofilename)
+float MidRapidityByPbeam(float pbeam);
+
+void recmap_pt_y_phi(std::string filelist, float pbeam=12.)
 {
   std::cout << "Macro started\n";
   
   const TString y_name = "y_{LAB}";
   const TString pT_name = "pT, GeV/c";
   const TString phi_name = "#varphi, rad";
-  const int y_nbins = 11;
-  const int pT_nbins = 13;
-  const int phi_nbins = 15;
-  const float y_beam = 1.62179;
-  const float y_low = y_beam - 0.8;
-  const float y_up = y_beam + 1.4;
+  const int y_nbins = 22;
+  const int pT_nbins = 26;
+  const int phi_nbins = 30;
+  const float midrapidity = MidRapidityByPbeam(pbeam);
+  const float y_low = midrapidity - 0.8;
+  const float y_up = midrapidity + 1.4;
   const float pT_low = 0.;
   const float pT_up = 2.6;
   const float phi_low = -TMath::Pi();
   const float phi_up = TMath::Pi();
+  
+  std::vector<int> pdgs{3122, 310};
 
-  TFile* reco_file = TFile::Open(recofilename);
-  TTree* reco_tree = reco_file->Get<TTree>("aTree");
+  AnalysisTree::Chain* reco_tree = new AnalysisTree::Chain(std::vector<std::string>({filelist}), std::vector<std::string>({"pTree"}));
   
   auto* reco_tracks = new AnalysisTree::Particles();
   
   reco_tree -> SetBranchAddress("Candidates", &reco_tracks);
   
-  TH2F hrec_y_pt("hrec_y_pt", "", y_nbins, y_low, y_up, pT_nbins, pT_low, pT_up);
-  TH2F hrec_phi_pt("hrec_phi_pt", "", phi_nbins, phi_low, phi_up, pT_nbins, pT_low, pT_up);
-  TH2F hrec_phi_y("hrec_phi_y", "", phi_nbins, phi_low, phi_up, y_nbins, y_low, y_up);
+  std::vector<TH2F*> hrec_y_pt, hrec_phi_pt, hrec_phi_y;
+  hrec_y_pt.resize(pdgs.size());
+  hrec_phi_pt.resize(pdgs.size());
+  hrec_phi_y.resize(pdgs.size());
   
-  hrec_y_pt.GetXaxis()->SetTitle(y_name);
-  hrec_phi_pt.GetXaxis()->SetTitle(phi_name);
-  hrec_phi_y.GetXaxis()->SetTitle(phi_name);
+  for(int i=0; i<pdgs.size(); i++)
+  {
+    hrec_y_pt.at(i) = new TH2F("hrec_y_pt", std::to_string(pdgs.at(i)).c_str(), y_nbins, y_low, y_up, pT_nbins, pT_low, pT_up);
+    hrec_phi_pt.at(i) = new TH2F("hrec_phi_pt", std::to_string(pdgs.at(i)).c_str(), y_nbins, y_low, y_up, pT_nbins, pT_low, pT_up);
+    hrec_phi_y.at(i) = new TH2F("hrec_phi_y", std::to_string(pdgs.at(i)).c_str(), y_nbins, y_low, y_up, pT_nbins, pT_low, pT_up);
+    
+    hrec_y_pt.at(i)->GetXaxis()->SetTitle(y_name);
+    hrec_phi_pt.at(i)->GetXaxis()->SetTitle(phi_name);
+    hrec_phi_y.at(i)->GetXaxis()->SetTitle(phi_name);
+    
+    hrec_y_pt.at(i)->GetYaxis()->SetTitle(pT_name);
+    hrec_phi_pt.at(i)->GetYaxis()->SetTitle(pT_name);
+    hrec_phi_y.at(i)->GetYaxis()->SetTitle(y_name);    
+  }
   
-  hrec_y_pt.GetYaxis()->SetTitle(pT_name);
-  hrec_phi_pt.GetYaxis()->SetTitle(pT_name);
-  hrec_phi_y.GetYaxis()->SetTitle(y_name);
+  const int generation_id = reco_tree->GetConfiguration()->GetBranchConfig("Candidates").GetFieldId("generation");
   
   const int rec_entries = reco_tree->GetEntries();
   for(int iEvent=0; iEvent<rec_entries; iEvent++)
   {
     reco_tree->GetEntry(iEvent);
+    if(iEvent%100==0)
+      std::cout << iEvent << std::endl;
     for(const auto& recotrack : *(reco_tracks->GetChannels()))
     {
-      if(recotrack.GetPid() != 3312) continue;
-      if(recotrack.GetField<int>(3) == 0) continue;
+      if(recotrack.GetField<int>(generation_id)==0) continue;
+      const int pid = recotrack.GetPid();
+      const int index = std::find(pdgs.begin(), pdgs.end(), pid)-pdgs.begin();
+      if (index>=pdgs.size()) continue;
       
-      hrec_y_pt.Fill(recotrack.GetRapidity(), recotrack.GetPt());
-      hrec_phi_pt.Fill(recotrack.GetPhi(), recotrack.GetPt());
-      hrec_phi_y.Fill(recotrack.GetPhi(), recotrack.GetRapidity());
-    }
+      hrec_y_pt.at(index)->Fill(recotrack.GetRapidity(), recotrack.GetPt());
+      hrec_phi_pt.at(index)->Fill(recotrack.GetPhi(), recotrack.GetPt());
+      hrec_phi_y.at(index)->Fill(recotrack.GetPhi(), recotrack.GetRapidity());
+    }    
   }
   
   TFile fileOut("recmap_pt_y_phi.root", "recreate");
-  fileOut.cd();
   
-  hrec_y_pt.Write();
-  hrec_phi_pt.Write();
-  hrec_phi_y.Write();
+  for(int i=0; i<pdgs.size(); i++)
+  {
+    fileOut.mkdir(std::to_string(pdgs.at(i)).c_str());
+    fileOut.cd(std::to_string(pdgs.at(i)).c_str());
+    
+    hrec_y_pt.at(i)->Write();
+    hrec_phi_pt.at(i)->Write();
+    hrec_phi_y.at(i)->Write();    
+  }
   
   fileOut.Close();
   
   std::cout << "Macro finished successfully\n";
+}
+
+float MidRapidityByPbeam(float pbeam)
+{
+  const float m = 0.938;
+  const float E = std::sqrt(m*m + pbeam*pbeam);
+  TLorentzVector v(0, 0, pbeam, E);
+  
+  return v.Rapidity()/2;
 }
   
--- QA/simmap_pt_y_phi.C
+++ QA/simmap_pt_y_phi.C
@@ -1,63 +1,99 @@
-void simmap_pt_y_phi(const TString simfilename)
+float MidRapidityByPbeam(float pbeam);
+
+void simmap_pt_y_phi(std::string filelist, float pbeam=12.)
 {
   std::cout << "Macro started\n";
   
   const TString y_name = "y_{LAB}";
   const TString pT_name = "pT, GeV/c";
   const TString phi_name = "#varphi, rad";
-  const int y_nbins = 11;
-  const int pT_nbins = 13;
-  const int phi_nbins = 15;
-  const float y_beam = 1.62179;
-  const float y_low = y_beam - 0.8;
-  const float y_up = y_beam + 1.4;
+  const int y_nbins = 22;
+  const int pT_nbins = 26;
+  const int phi_nbins = 30;
+  const float midrapidity = MidRapidityByPbeam(pbeam);
+  const float y_low = midrapidity - 0.8;
+  const float y_up = midrapidity + 1.4;
   const float pT_low = 0.;
   const float pT_up = 2.6;
   const float phi_low = -TMath::Pi();
   const float phi_up = TMath::Pi();
   
-  TFile* sim_file = TFile::Open(simfilename);
-  TTree* sim_tree = sim_file->Get<TTree>("rTree");
+  std::cout << midrapidity << std::endl;
+  
+  std::vector<int> pdgs{3122, 310};
+  
+  AnalysisTree::Chain* sim_tree = new AnalysisTree::Chain(std::vector<std::string>({filelist}), std::vector<std::string>({"aTree"}));
   
   auto* sim_tracks = new AnalysisTree::Particles();
   
   sim_tree -> SetBranchAddress("SimParticles", &sim_tracks);
   
-  TH2F hsim_y_pt("hsim_y_pt", "", y_nbins, y_low, y_up, pT_nbins, pT_low, pT_up);
-  TH2F hsim_phi_pt("hsim_phi_pt", "", phi_nbins, phi_low, phi_up, pT_nbins, pT_low, pT_up);
-  TH2F hsim_phi_y("hsim_phi_y", "", phi_nbins, phi_low, phi_up, y_nbins, y_low, y_up);
+  std::vector<TH2F*> hsim_y_pt, hsim_phi_pt, hsim_phi_y;
+  hsim_y_pt.resize(pdgs.size());
+  hsim_phi_pt.resize(pdgs.size());
+  hsim_phi_y.resize(pdgs.size());
   
-  hsim_y_pt.GetXaxis()->SetTitle(y_name);
-  hsim_phi_pt.GetXaxis()->SetTitle(phi_name);
-  hsim_phi_y.GetXaxis()->SetTitle(phi_name);
+  for(int i=0; i<pdgs.size(); i++)
+  {
+    hsim_y_pt.at(i) = new TH2F("hsim_y_pt", std::to_string(pdgs.at(i)).c_str(), y_nbins, y_low, y_up, pT_nbins, pT_low, pT_up);
+    hsim_phi_pt.at(i) = new TH2F("hsim_phi_pt", std::to_string(pdgs.at(i)).c_str(), y_nbins, y_low, y_up, pT_nbins, pT_low, pT_up);
+    hsim_phi_y.at(i) = new TH2F("hsim_phi_y", std::to_string(pdgs.at(i)).c_str(), y_nbins, y_low, y_up, pT_nbins, pT_low, pT_up);
+    
+    hsim_y_pt.at(i)->GetXaxis()->SetTitle(y_name);
+    hsim_phi_pt.at(i)->GetXaxis()->SetTitle(phi_name);
+    hsim_phi_y.at(i)->GetXaxis()->SetTitle(phi_name);
+    
+    hsim_y_pt.at(i)->GetYaxis()->SetTitle(pT_name);
+    hsim_phi_pt.at(i)->GetYaxis()->SetTitle(pT_name);
+    hsim_phi_y.at(i)->GetYaxis()->SetTitle(y_name);    
+  }
   
-  hsim_y_pt.GetYaxis()->SetTitle(pT_name);
-  hsim_phi_pt.GetYaxis()->SetTitle(pT_name);
-  hsim_phi_y.GetYaxis()->SetTitle(y_name);
+  const int mother_id_id = sim_tree->GetConfiguration()->GetBranchConfig("SimParticles").GetFieldId("mother_id");
   
   const int sim_entries = sim_tree->GetEntries();
   for(int iEvent=0; iEvent<sim_entries; iEvent++)
   {
     sim_tree->GetEntry(iEvent);
+    if(iEvent%100==0)
+      std::cout << iEvent << std::endl;
     for(const auto& simtrack : *(sim_tracks->GetChannels()))
     {
-      if(simtrack.GetPid() != 3312) continue;
+      const int mother_id = simtrack.GetField<int>(mother_id_id);
+      if(mother_id != -1) continue;
       
-      hsim_y_pt.Fill(simtrack.GetRapidity(), simtrack.GetPt());
-      hsim_phi_pt.Fill(simtrack.GetPhi(), simtrack.GetPt());
-      hsim_phi_y.Fill(simtrack.GetPhi(), simtrack.GetRapidity());
+      const int pid = simtrack.GetPid();
+      const int index = std::find(pdgs.begin(), pdgs.end(), pid)-pdgs.begin();
+      if (index>=pdgs.size()) continue;
+      
+      hsim_y_pt.at(index)->Fill(simtrack.GetRapidity(), simtrack.GetPt());
+      hsim_phi_pt.at(index)->Fill(simtrack.GetPhi(), simtrack.GetPt());
+      hsim_phi_y.at(index)->Fill(simtrack.GetPhi(), simtrack.GetRapidity());
     }    
   }
   
   TFile fileOut("simmap_pt_y_phi.root", "recreate");
-  fileOut.cd();
   
-  hsim_y_pt.Write();
-  hsim_phi_pt.Write();
-  hsim_phi_y.Write();
+  for(int i=0; i<pdgs.size(); i++)
+  {
+    fileOut.mkdir(std::to_string(pdgs.at(i)).c_str());
+    fileOut.cd(std::to_string(pdgs.at(i)).c_str());
+    
+    hsim_y_pt.at(i)->Write();
+    hsim_phi_pt.at(i)->Write();
+    hsim_phi_y.at(i)->Write();    
+  }
   
   fileOut.Close();
   
   std::cout << "Macro finished successfully\n";
+}
+
+float MidRapidityByPbeam(float pbeam)
+{
+  const float m = 0.938;
+  const float E = std::sqrt(m*m + pbeam*pbeam);
+  TLorentzVector v(0, 0, pbeam, E);
+  
+  return v.Rapidity()/2;
 }
   
